c4c2ac57b5607de9b832b3392d5b1378ea97b971
Andrejs Eisaks <eisaks.andrejs@gmail.com>
2023-03-15 15:20:01 +0100

    Add keybindings for: window management, tab management and nvim-tree plugin

--- lua/coderxin/core/keymap.lua
+++ lua/coderxin/core/keymap.lua
@@ -6,6 +6,17 @@ local keymap = vim.keymap
 -- General Keymaps --
 keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
+keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
+
 -- Plugin Keymaps --
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
@@ -18,3 +29,7 @@ local builtin = require('telescope.builtin')
 keymap.set('n', "<leader>fg", function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
+-- nvim-tree
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
+
