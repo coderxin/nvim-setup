a26512910cba90f76482806f59934eecd6e122ce
Andrejs Eisaks <eisaks.andrejs@gmail.com>
2023-03-21 22:43:26 +0100

    Setup of "Format on save"

--- lua/coderxin/core/options.lua
+++ lua/coderxin/core/options.lua
@@ -2,16 +2,16 @@ local opt = vim.opt
 
 -- Line numbers
 opt.relativenumber = true -- Show relative line numbers
-opt.nu = true -- Show absolute line number (when relative number is on)
+opt.nu = true             -- Show absolute line number (when relative number is on)
 
 -- Tabs
-opt.tabstop = 2 -- Spaces for tabs
-opt.softtabstop = 2 -- How many columns (=spaces) the cursor moves right when you press <Tab>
-opt.shiftwidth = 2 -- Space for ident width
+opt.tabstop = 2      -- Spaces for tabs
+opt.softtabstop = 2  -- How many columns (=spaces) the cursor moves right when you press <Tab>
+opt.shiftwidth = 2   -- Space for ident width
 opt.expandtab = true -- Expand tab to spaces
 
 -- Identation
-opt.autoindent = true -- Copy ident from current line when starting a new one
+opt.autoindent = true  -- Copy ident from current line when starting a new one
 opt.smartindent = true -- Reacts to the syntax/style of the code you are editing
 
 -- Line wrapping
@@ -22,9 +22,9 @@ opt.colorcolumn = "80" -- Highlighted screen column
 
 -- Search settings
 opt.ignorecase = true -- Ignore case when searching
-opt.smartcase = true -- Case-insentitive if the search contains upper case chars
-opt.hlsearch = false --  Do not highlight previous search matches 
-opt.incsearch = true -- Show where the pattern, as it was typed so far, matches
+opt.smartcase = true  -- Case-insentitive if the search contains upper case chars
+opt.hlsearch = false  --  Do not highlight previous search matches
+opt.incsearch = true  -- Show where the pattern, as it was typed so far, matches
 
 -- Appearance
 opt.termguicolors = true
@@ -35,9 +35,11 @@ opt.scrolloff = 8
 opt.clipboard:append("unnamedplus") -- Use system clipboard as default register
 
 -- Misc
-opt.updatetime = 50
+opt.updatetime = 350
 opt.swapfile = false
 opt.backup = false
 opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 opt.undofile = true
 
+-- Format on Save
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
