abbd18ca8538a209754341b5f9ba7d6dad4b8253
Andrejs Eisaks <eisaks.andrejs@gmail.com>
2023-03-30 16:14:11 +0200

    Add treesitter for bash

--- lua/coderxin/plugins/treesitter.lua
+++ lua/coderxin/plugins/treesitter.lua
@@ -5,18 +5,14 @@ end
 
 treesitter.setup({
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "help", "javascript", "typescript", "ruby", "rust", "lua", "vim", "query" },
-
+  ensure_installed = { "help", "javascript", "typescript", "ruby", "rust", "lua", "vim", "query", "bash" },
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
-
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
-
   highlight = {
     enable = true,
-
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
@@ -24,4 +20,3 @@ treesitter.setup({
     additional_vim_regex_highlighting = false,
   },
 })
-
