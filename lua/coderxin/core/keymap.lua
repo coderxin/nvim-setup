23964fcc40e88e9bcbf3ba0c5b3b22f1004f6bf6
Andrejs Eisaks <eisaks.andrejs@gmail.com>
2023-03-30 15:58:39 +0200

    Add keymap to move text up and down in visual mode

--- lua/coderxin/core/keymap.lua
+++ lua/coderxin/core/keymap.lua
@@ -11,6 +11,10 @@ keymap.set("n", "<leader>w", ":w<CR>")
 keymap.set("n", "<leader>q", ":q<CR>")
 keymap.set("n", "<leader>qa", ":qa<CR>")
 
+-- Move up and down
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 -- Escape
 keymap.set("i", "jj", "<ESC>", { silent = true })
 
@@ -22,7 +26,6 @@ keymap.set("n", "<leader>sv", "<C-w>v")        -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s")        -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=")        -- make split windows equal width & height
 keymap.set("n", "<leader>sx", ":close<CR>")    -- close current split window
-
 keymap.set("n", "<leader>to", ":tabnew<CR>")   -- open new tab
 keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>")     -- go to next tab
