9ae91d2e775fdf487f34cacaf76549dfa06e5b0d
Andrejs Eisaks <eisaks.andrejs@gmail.com>
2023-03-31 14:07:57 +0200

    Add dedicated binding for the rust-tools code_action_group

--- lua/coderxin/plugins/rust-tools.lua
+++ lua/coderxin/plugins/rust-tools.lua
@@ -5,15 +5,14 @@ if not rust_tools_setup then
 end
 
 rust_tools.setup({
-    standalone = false,
-    root_dir = require('lspconfig').util.find_git_ancestor,
-    server = {
-        on_attach = function(_, bufnr)
-            -- Hover actions
-            vim.keymap.set("n", "<C-space>", rust_tools.hover_actions.hover_actions, { buffer = bufnr })
-            -- Code action groups
-            vim.keymap.set("n", "<Leader>a", rust_tools.code_action_group.code_action_group, { buffer = bufnr })
-        end,
-    },
+  standalone = false,
+  root_dir = require('lspconfig').util.find_git_ancestor,
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rust_tools.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>ra", rust_tools.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
 })
-
