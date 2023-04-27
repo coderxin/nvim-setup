f84a3799af6b7aebbd4096294b10fc81198b6386
Andrejs Eisaks <eisaks.andrejs@gmail.com>
2023-04-27 23:59:52 +0300

    Use tokyonight theme + lualine

--- lua/coderxin/plugins-setup.lua
+++ lua/coderxin/plugins-setup.lua
@@ -42,10 +42,10 @@ return packer.startup(function(use)
 
   -- colorscheme
   use({
-    "sainnhe/edge",
+    "folke/tokyonight.nvim",
     config = function()
       vim.cmd("set termguicolors")
-      vim.cmd("colorscheme edge")
+      vim.cmd("colorscheme tokyonight")
     end
   })
 
