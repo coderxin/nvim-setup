feef63166423dd279e54fda4f4f116adae42a1a0
Andrejs Eisaks <eisaks.andrejs@gmail.com>
2023-05-02 01:59:10 +0200

    Add LSP servers

--- lua/coderxin/plugins/lsp.lua
+++ lua/coderxin/plugins/lsp.lua
@@ -3,10 +3,19 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-  'tsserver',
   'lua_ls',
   'ruby_ls',
   'rust_analyzer',
+  'eslint',
+  'jsonls',
+  'yamlls',
+  'terraformls',
+  'tflint',
+  'dockerls',
+  'bashls',
+  'marksman',
+  'html',
+  'graphql',
 })
 
 -- Fix Undefined global 'vim'
@@ -46,10 +55,11 @@ lsp.set_preferences({
   }
 })
 
-lsp.on_attach(function(client, bufnr)
+lsp.on_attach(function(_, bufnr)
   local opts = { buffer = bufnr, remap = false }
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "<space>D", function() vim.lsp.buf.type_definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
@@ -58,7 +68,10 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("i", "<C-k>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set('n', '<space>f', function()
+    vim.lsp.buf.format { async = true }
+  end, opts)
 end)
 
 lsp.setup()
